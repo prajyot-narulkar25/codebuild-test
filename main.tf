f1fccef7735b71be098f1ea06d48040742543509
Prajyot Narulkar <30620642+prajyot-narulkar25@users.noreply.github.com>
2025-02-13 16:29:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 # Provisions a fake VPC using the Fake Web Services provider.
 
-# resource "fakewebservices_vpc" "primary_vpc" {
-#   name       = "Primary VPC"
-#   cidr_block = "10.1.0.0/16"
-# }
+resource "fakewebservices_vpc" "primary_vpc" {
+    name       = "Primary VPC"
+    cidr_block = "10.1.0.0/16"
+}
